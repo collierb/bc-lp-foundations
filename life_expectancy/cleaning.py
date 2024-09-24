5b6a70a7fc9a4ca8a6a3ad95e65b5dd04b6da434
collierb <collierben3@gmail.com>
2024-09-24 15:49:33 +0100

    args added to cleaning file

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,16 +1,28 @@
+'''preprocess data'''
 import pandas as pd
+import argparse
 
-def clean_data():
-    df = pd.read_csv('data/eu_life_expectancy_raw.tsv', sep='\t')
+def clean_data(country):
+    '''function to load, process and save data'''
+    abs_path = 'C:/Users/colli/Documents/dareData/assignments/life_expectancy/'
+    df = pd.read_csv(abs_path + 'data/eu_life_expectancy_raw.tsv', sep='\t')
     df[['unit','sex','age','region']] = df['unit,sex,age,geo\\time'].str.split(',', expand=True)
     df.drop(columns='unit,sex,age,geo\\time', inplace=True)
-    long_df = pd.melt(df, id_vars=['unit','sex','age','region'], var_name='year', value_name='value')
+    long_df = pd.melt(df,
+                      id_vars=['unit','sex','age','region'],
+                      var_name='year',
+                      value_name='value')
     long_df['year'] = long_df.year.str.strip().astype(int)
     long_df['value'] = long_df.value.str.split(expand=True)[0]
     long_df['value'] = pd.to_numeric(long_df.value, errors='coerce')
     long_df.dropna(inplace=True)
     long_df.reset_index(inplace=True, drop=True)
-    long_df[long_df['region']=='PT'].reset_index(drop=True).to_csv('data/pt_life_expectancy.csv', index=False)
-    print('clean and filtered file saved to data folder')
+    pt_df = long_df[long_df['region']==country].reset_index(drop=True)
+    pt_df.to_csv(abs_path + f'data/{country}_life_expectancy.csv', index=False)
+    print(f'clean and filtered file for {country} saved to data folder')
 
-clean_data()
+if __name__ == "__main__": # pragma: no cover
+    parser = argparse.ArgumentParser(description='country filter for life expectancy data')
+    parser.add_argument('--country', default='PT', help='select country for filter in ISO2 format eg FR')
+    args = parser.parse_args()                
+    clean_data(args.country)
