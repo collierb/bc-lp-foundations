2ffbfcdc21171f9b4d4af0b074829af9b6926d75
collierb <collierben3@gmail.com>
2024-09-30 12:19:08 +0100

    refactoring large clean function

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -3,28 +3,49 @@ from pathlib import Path
 import argparse
 import pandas as pd
 
-PATH = Path(__file__).parent
+PARENT_PATH = Path(__file__).parent
 
-def clean_data(country):
-    '''function to load, process and save data'''
-    df = pd.read_csv(PATH/'data'/'eu_life_expectancy_raw.tsv', sep='\t')
-    df[['unit','sex','age','region']] = df['unit,sex,age,geo\\time'].str.split(',', expand=True)
-    df.drop(columns='unit,sex,age,geo\\time', inplace=True)
-    long_df = pd.melt(df,
+def load_data() -> pd.DataFrame:
+    '''load raw life expectancy data file stored in data folder'''
+    df = pd.read_csv(PARENT_PATH/'data'/'eu_life_expectancy_raw.tsv', sep='\t')
+    return df
+
+def clean_data(loaded_df: pd.DataFrame) -> pd.DataFrame:
+    '''clean data and reshape to long format.'''
+    loaded_df[['unit','sex','age','region']] = loaded_df['unit,sex,age,geo\\time'].str.split(',', expand=True)
+    # tab seperation from the load data function above left one column needing to be split on the comma
+    loaded_df.drop(columns='unit,sex,age,geo\\time', inplace=True)
+    long_df = pd.melt(loaded_df,
                       id_vars=['unit','sex','age','region'],
                       var_name='year',
                       value_name='value')
     long_df['year'] = long_df.year.str.strip().astype(int)
-    long_df['value'] = long_df.value.str.split(expand=True)[0]
-    long_df['value'] = pd.to_numeric(long_df.value, errors='coerce')
+    long_df['value'] = long_df.value.str.split(expand=True)[0] 
+    # whitespace seperated out various non-numeric references in variable value
+    long_df['value'] = pd.to_numeric(long_df.value, errors='coerce') # coerce ensures all "" entries converted to NaN
     long_df.dropna(inplace=True)
     long_df.reset_index(inplace=True, drop=True)
-    country_df = long_df[long_df['region']==country].reset_index(drop=True)
-    country_df.to_csv(PATH/'data'/f'{country}_life_expectancy.csv', index=False)
+    return long_df
+    
+
+def save_data(cleaned_df: pd.DataFrame, country: str) -> None:
+    '''save the cleaned data to data folder'''
+    country_df = cleaned_df[cleaned_df['region']==country].reset_index(drop=True)
+    country_df.to_csv(PARENT_PATH/'data'/f'{country}_life_expectancy.csv', index=False)
     print(f'clean and filtered file for {country} saved to data folder')
+    
+
+def preprocess_data(country: str) -> None:
+    '''call load, clean and save functions.
+    Optional country selection defauts to PT
+    '''
+    loaded_df = load_data()
+    cleaned_df = clean_data(loaded_df)
+    save_data(cleaned_df, country)
+
 
 if __name__ == "__main__": # pragma: no cover
     parser = argparse.ArgumentParser(description='country filter for life expectancy data')
     parser.add_argument('--country', default='PT', help='select country in ISO2 format eg FR')
     args = parser.parse_args()
-    clean_data(args.country)
+    preprocess_data(args.country)
